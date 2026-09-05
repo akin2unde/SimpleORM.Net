9317cd79de1057a0540a2951823cd25f6ddffbbd
Akintunde <akin2unde@gmail.com>
2026-09-05 18:36:30 +0100

    Mongo db query generator fix

---
+++ src/SimpleORM.Net/KeyValue.cs
@@ -0,0 +1,25 @@
+namespace SimpleORM.Net.Configuration;
+
+/// <summary>Util class.</summary>
+public sealed class KeyValue
+{
+
+    /// <summary>Key.</summary>
+    public required string Key
+    {
+        get;
+
+        set;
+
+    }
+    /// <summary>Value.</summary>
+
+    public object? Value
+    {
+        get;
+
+        set;
+
+    }
+
+}
